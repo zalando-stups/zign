13462c9dcc57f26b57cbc54c3f9015b2c42bc4fb
Henning Jacobs <henning@jacobs1.de>
2015-05-13 15:25:24 +0200

    request token

--- zign/cli.py
+++ zign/cli.py
@@ -1,18 +1,23 @@
 import click
 import os
 import keyring
-import yaml
+import requests
 import time
+import yaml
 
 import zign
 
-from clickclick import Action, choice, error, AliasedGroup, info, print_table
+from clickclick import error, AliasedGroup, print_table, OutputFormat
 
+KEYRING_KEY = 'zign'
 CONFIG_DIR_PATH = click.get_app_dir('zign')
 CONFIG_FILE_PATH = os.path.join(CONFIG_DIR_PATH, 'zign.yaml')
-LAST_UPDATE_FILE_PATH = os.path.join(CONFIG_DIR_PATH, 'last_update.yaml')
+TOKENS_FILE_PATH = os.path.join(CONFIG_DIR_PATH, 'tokens.yaml')
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+output_option = click.option('-o', '--output', type=click.Choice(['text', 'json', 'tsv']), default='text',
+                             help='Use alternative output format')
 
 
 def print_version(ctx, param, value):
@@ -38,18 +43,83 @@ def cli(ctx, config_file):
 
 
 @cli.command('list')
+@output_option
 @click.pass_obj
-def list_tokens(obj):
+def list_tokens(obj, output):
     '''List tokens'''
+    try:
+        with open(TOKENS_FILE_PATH) as fd:
+            data = yaml.safe_load(fd)
+    except:
+        data = {}
 
-    pass
+    rows = []
+    for key, val in sorted(data.items()):
+        rows.append({'name': key, 'token': val.get('access_token'), 'creation_time': val.get('creation_time')})
+
+    with OutputFormat(output):
+        print_table('name access_token creation_time'.split(), rows)
 
 
 @cli.command('token')
+@click.argument('scope', nargs=-1)
+@click.option('--url', help='URL to generate access token', metavar='URI')
+@click.option('--realm', help='Use custom OAuth2 realm', metavar='NAME')
+@click.option('-n', '--name', help='Custom token name', metavar='TOKEN_NAME')
+@click.option('-U', '--user', help='Username to use for authentication', envvar='USER', metavar='NAME')
+@click.option('-p', '--password', help='Password to use for authentication', envvar='ZIGN_PASSWORD', metavar='PWD')
 @click.pass_obj
-def create_token(obj):
+def create_token(obj, scope, url, realm, name, user, password):
     '''Create a new token'''
-    pass
+
+    config = obj
+
+    while not url:
+        url = click.prompt('Please enter the OAuth access token service URL')
+        if not url.startswith('http'):
+            url = 'https://{}'.format(url)
+
+        try:
+            requests.get(url, timeout=5)
+        except:
+            error('Could not reach {}'.format(url))
+            url = None
+
+        config['url'] = url
+
+    os.makedirs(CONFIG_DIR_PATH, exist_ok=True)
+    with open(CONFIG_FILE_PATH, 'w') as fd:
+        yaml.dump(config, fd)
+
+    password = password or keyring.get_password(KEYRING_KEY, user)
+
+    if not password:
+        password = click.prompt('Password', hide_input=True)
+
+    params = {'json': 'true'}
+    if realm:
+        params['realm'] = realm
+    response = requests.get(url, params=params, auth=(user, password))
+
+    if response.status_code == 200:
+        keyring.set_password(KEYRING_KEY, user, password)
+
+    result = response.json()
+
+    if name:
+        try:
+            with open(TOKENS_FILE_PATH) as fd:
+                data = yaml.safe_load(fd)
+        except:
+            data = {}
+
+        data[name] = result
+        data[name]['creation_time'] = time.time()
+
+        with open(TOKENS_FILE_PATH, 'w') as fd:
+            yaml.safe_dump(data, fd)
+
+    print(result.get('access_token'))
 
 
 def main():
