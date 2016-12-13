14250cfbcb1fee658a5c065de27558b52ee7c38d
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2016-12-13 10:28:15 +0100

    Removed deprecation warning

--- zign/cli_zign.py
+++ zign/cli_zign.py
@@ -62,5 +62,4 @@ def token(obj, scope, url, realm, name, user, password, insecure, refresh):
 
 
 def main():
-    click.secho('WARNING: zign is deprecated! Use ztoken instead.', err=True, fg='red')
     cli_zign()
