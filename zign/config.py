e08812cc3559a5d87b67787b07c2456f2902f518
Henning Jacobs <henning@zalando.de>
2016-12-15 11:36:42 +0100

    backwards compatible get_config()

--- zign/config.py
+++ zign/config.py
@@ -2,6 +2,7 @@ import os
 import click
 
 KEYRING_KEY = 'zign'
+OLD_CONFIG_NAME = 'zign'
 CONFIG_NAME = 'zalando-token-cli'
 CONFIG_DIR_PATH = click.get_app_dir(CONFIG_NAME)
 TOKENS_FILE_PATH = os.path.join(CONFIG_DIR_PATH, 'tokens.yaml')
