8e189ae07f9d0e0edb72e8bf1064327bdfc236c7
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2017-05-26 18:04:20 +0200

    #50 fixed mocked objects in deprecated zign calls

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,11 +4,16 @@ from zign.cli import cli
 
 
 def test_token(monkeypatch):
+
     token = 'abc-123'
 
-    get_token_implicit_flow = MagicMock()
-    get_token_implicit_flow.return_value = {'access_token': token, 'expires_in': 1, 'token_type': 'test'}
-    monkeypatch.setattr('zign.cli.get_token_implicit_flow', get_token_implicit_flow)
+    load_config = MagicMock()
+    load_config.return_value = {'authorize_url': 'https://localhost/authorize', 'token_url': 'https://localhost/token', 'client_id': 'foobar', 'business_partner_id': '123'}
+    perform_implicit_flow = MagicMock()
+    perform_implicit_flow.return_value = {'access_token': token, 'expires_in': 1, 'token_type': 'test'}
+
+    monkeypatch.setattr('zign.api.perform_implicit_flow', perform_implicit_flow)
+    monkeypatch.setattr('stups_cli.config.load_config', load_config)
 
     runner = CliRunner()
 
@@ -16,9 +21,6 @@ def test_token(monkeypatch):
         result = runner.invoke(cli, ['token', '-n', 'mytok', '--refresh'], catch_exceptions=False)
 
         assert token == result.output.rstrip().split('\n')[-1]
-        get_token_implicit_flow.assert_called_with('mytok', authorize_url=None, token_url=None,
-                                                   business_partner_id=None, client_id=None, refresh=True)
-
 
 def test_token_with_refresh_url(monkeypatch):
     token = 'abc-123'
--- tests/test_cli_zign.py
+++ tests/test_cli_zign.py
@@ -7,26 +7,30 @@ from zign.cli_zign import cli_zign
 def test_create_list_delete(monkeypatch):
     token = 'abc-123'
 
-    get_token_implicit_flow = MagicMock()
-    get_token_implicit_flow.return_value = {'access_token': token, 'expires_in': 1, 'token_type': 'test'}
-    monkeypatch.setattr('zign.api.perform_implicit_flow', get_token_implicit_flow)
+    perform_implicit_flow = MagicMock()
+    perform_implicit_flow.return_value = {'access_token': token, 'expires_in': 1, 'token_type': 'test'}
+    monkeypatch.setattr('zign.api.perform_implicit_flow', perform_implicit_flow)
+
+    load_config = MagicMock()
+    load_config.return_value = {'authorize_url': 'https://localhost/authorize', 'token_url': 'https://localhost/token', 'client_id': 'foobar', 'business_partner_id': '123'}
+    monkeypatch.setattr('stups_cli.config.load_config', load_config)
 
     runner = CliRunner()
 
     with runner.isolated_filesystem():
         result = runner.invoke(cli_zign, ['token', '-n', 'mytok', '--password', 'mypass'], catch_exceptions=False)
 
-#        assert token == result.output.rstrip().split('\n')[-1]
-#
-#        result = runner.invoke(cli_zign, ['list', '-o', 'json'], catch_exceptions=False)
-#        data = json.loads(result.output)
-#        assert len(data) >= 1
-#        assert 'mytok' in [r['name'] for r in data]
-#
-#        result = runner.invoke(cli_zign, ['delete', 'mytok'], catch_exceptions=False)
-#        result = runner.invoke(cli_zign, ['list', '-o', 'json'], catch_exceptions=False)
-#        data = json.loads(result.output)
-#        assert 'mytok' not in [r['name'] for r in data]
-#
-#        # should work again for already deleted tokens
-#        result = runner.invoke(cli_zign, ['delete', 'mytok'], catch_exceptions=False)
+        assert token == result.output.rstrip().split('\n')[-1]
+
+        result = runner.invoke(cli_zign, ['list', '-o', 'json'], catch_exceptions=False)
+        data = json.loads(result.output)
+        assert len(data) >= 1
+        assert 'mytok' in [r['name'] for r in data]
+
+        result = runner.invoke(cli_zign, ['delete', 'mytok'], catch_exceptions=False)
+        result = runner.invoke(cli_zign, ['list', '-o', 'json'], catch_exceptions=False)
+        data = json.loads(result.output)
+        assert 'mytok' not in [r['name'] for r in data]
+
+        # should work again for already deleted tokens
+        result = runner.invoke(cli_zign, ['delete', 'mytok'], catch_exceptions=False)
