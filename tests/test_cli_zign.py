36fd2c7128a31eb3d4d697cbbff88c5538c170d9
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2017-05-26 14:46:25 +0200

    #50 Uncommented everything, added MonkeyPatch for implicit flow

--- tests/test_cli_zign.py
+++ tests/test_cli_zign.py
@@ -7,7 +7,9 @@ from zign.cli_zign import cli_zign
 def test_create_list_delete(monkeypatch):
     token = 'abc-123'
 
-    monkeypatch.setattr('zign.api.perform_implicit_flow', lambda a: {'access_token': token, 'expires_in': 1, 'token_type': 'test'})
+    get_token_implicit_flow = MagicMock()
+    get_token_implicit_flow.return_value = {'access_token': token, 'expires_in': 1, 'token_type': 'test'}
+    monkeypatch.setattr('zign.api.perform_implicit_flow', get_token_implicit_flow)
 
     runner = CliRunner()
 
@@ -15,16 +17,16 @@ def test_create_list_delete(monkeypatch):
         result = runner.invoke(cli_zign, ['token', '-n', 'mytok', '--password', 'mypass'], catch_exceptions=False)
 
         assert token == result.output.rstrip().split('\n')[-1]
-#
-#        result = runner.invoke(cli_zign, ['list', '-o', 'json'], catch_exceptions=False)
-#        data = json.loads(result.output)
-#        assert len(data) >= 1
-#        assert 'mytok' in [r['name'] for r in data]
-#
-#        result = runner.invoke(cli_zign, ['delete', 'mytok'], catch_exceptions=False)
-#        result = runner.invoke(cli_zign, ['list', '-o', 'json'], catch_exceptions=False)
-#        data = json.loads(result.output)
-#        assert 'mytok' not in [r['name'] for r in data]
-#
-#        # should work again for already deleted tokens
-#        result = runner.invoke(cli_zign, ['delete', 'mytok'], catch_exceptions=False)
+
+        result = runner.invoke(cli_zign, ['list', '-o', 'json'], catch_exceptions=False)
+        data = json.loads(result.output)
+        assert len(data) >= 1
+        assert 'mytok' in [r['name'] for r in data]
+
+        result = runner.invoke(cli_zign, ['delete', 'mytok'], catch_exceptions=False)
+        result = runner.invoke(cli_zign, ['list', '-o', 'json'], catch_exceptions=False)
+        data = json.loads(result.output)
+        assert 'mytok' not in [r['name'] for r in data]
+
+        # should work again for already deleted tokens
+        result = runner.invoke(cli_zign, ['delete', 'mytok'], catch_exceptions=False)
